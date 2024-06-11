14664c07f9f29d8d594f1c67821e25a690b2d4e4
Dr-AddictStein <dr.addictstein@gmail.com>
2024-06-12 02:16:14 +0600

    file-upload-done.!.

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -20,3 +20,22 @@ export const createImage = async (req, res) => {
         res.status(403).json({ status: false, error: error })
     }
 }
+
+export const createSourceCode = async (req, res) => {
+    if (!req.file) {
+        res.status(403).json({ status: false, error: "Please upload a file" });
+        return;
+    }
+    const data = {
+        url: req.file.location,
+        type: req.file.mimetype
+    };
+    try {
+        res.send({
+            data: data,
+            status: true
+        });
+    } catch (error) {
+        res.status(403).json({ status: false, error: error.message });
+    }
+}
--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,14 +1,13 @@
 import express from 'express';
 
 import uploadMiddleWare from "../middlewares/fileUpload.js";
-import { createImage } from '../controllers/fileController.js';
+import { createImage, createSourceCode } from '../controllers/fileController.js';
 import uploadZipMiddleWare from "../middlewares/sourceCodeUpload.js";
-import { createSourceCode } from '../controllers/sourceCodeController.js';
 
 
 const router = express.Router();
 
-router.post('/image/:labId',uploadMiddleWare.single("file"),createImage);
+router.post('/image/:labId',uploadMiddleWare.single("file"), createImage);
 router.post('/source_code/:labId', uploadZipMiddleWare.single("file"), createSourceCode);
 
 export default router;
