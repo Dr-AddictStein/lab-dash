071c51688062d7eab82e0ea9f72c509c38ed25a5
Dr-AddictStein <dr.addictstein@gmail.com>
2024-06-12 02:24:55 +0600

    step-image-upload-done.!.

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -39,3 +39,22 @@ export const createSourceCode = async (req, res) => {
         res.status(403).json({ status: false, error: error.message });
     }
 }
+
+export const createStepImage = async (req, res) => {
+    if (!req.file) {
+        res.status(403).json({ status: false, error: "Please upload a file" });
+        return;
+    }
+    const data = {
+        url: req.file.location,
+        type: req.file.mimetype
+    };
+    try {
+        res.send({
+            data: data,
+            status: true
+        });
+    } catch (error) {
+        res.status(403).json({ status: false, error: error.message });
+    }
+}
--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,14 +1,16 @@
 import express from 'express';
 
 import uploadMiddleWare from "../middlewares/fileUpload.js";
-import { createImage, createSourceCode } from '../controllers/fileController.js';
 import uploadZipMiddleWare from "../middlewares/sourceCodeUpload.js";
+import uploadMiddleWareStepImage from '../middlewares/stepImageUpload.js';
+import { createImage, createSourceCode, createStepImage } from '../controllers/fileController.js';
 
 
 const router = express.Router();
 
 router.post('/image/:labId',uploadMiddleWare.single("file"), createImage);
 router.post('/source_code/:labId', uploadZipMiddleWare.single("file"), createSourceCode);
+router.post('/stepImage/:labId/:stepId', uploadMiddleWareStepImage.single("file"), createStepImage);
 
 export default router;
 
